34eb5262740496ff364b17ee5be4cd2d57cf603d
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 20:23:12 +0800

    Version 1.0

--- SwiftUI Alerts/ContentView.swift
+++ SwiftUI Alerts/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsPresented = false
+    @State private var backgroundUpdated = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                if backgroundUpdated {
+                    Color.red
+                }
+                else {
+                    Color.blue
+                }
+                
+                VStack {
+                    Button(action: {
+                        self.alertIsPresented = true
+                    }, label: {
+                        Text("Tap Me!")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 24))
+                    })
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+                    .alert(isPresented: $alertIsPresented, content: {
+                        Alert(title: Text("Would you like to purchase?"),
+                              primaryButton: .default(Text("Purchase"), action: {
+                                self.backgroundUpdated.toggle()
+                              }),
+                              secondaryButton: .cancel(Text("No Thanks")))
+                    })
+                }
+                .navigationTitle("SwiftUI Alerts")
+            }
+        }
     }
 }
 
