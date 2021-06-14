0cf23eb8b0a1232e6f71097336f34f2041338593
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 19:46:20 +0800

    Initial Commit

---
+++ SwiftUI Alerts/SwiftUI_AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AlertsApp.swift
+//  SwiftUI Alerts
+//
+//  Created by Amben on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
